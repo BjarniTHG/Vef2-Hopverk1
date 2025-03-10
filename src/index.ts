ea4d48b68f50cd7f1864fdc1f906a6b430cb7f3b
Klingsterina <eyglo2001@gmail.com>
2025-03-10 21:42:34 +0000

    meira setup stuff

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
+export const app = new Hono()
+app.use('/*', cors())
+
+app.get('/', (c) => {
+	const data: { message: string } = {
+		message: 'Hopverkefni 1 í veforritun 2'
+	}
+	return c.json(data)
+})
+
+serve({
+	fetch: app.fetch,
+	port: 3000
+}, (info) => {
+	console.log(`Server is running on http://localhost:${info.port}`)
+})
